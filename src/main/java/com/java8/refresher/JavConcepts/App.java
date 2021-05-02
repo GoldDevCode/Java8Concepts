459c4e318df693c0be4d2f83423ae95c90dd3794
Swarnim <swarnim2008@gmail.com>
2021-05-02 17:48:06 +0200

    Initial Commit to Repository

---
+++ src/main/java/com/java8/refresher/JavConcepts/App.java
@@ -0,0 +1,13 @@
+package com.java8.refresher.JavConcepts;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
