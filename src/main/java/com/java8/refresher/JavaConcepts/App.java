2c7c98afffb4cbcdcc68dbbe7f74b1536d448a0d
GoldDevCode <swarnimb6@gmail.com>
2021-05-03 15:08:42 +0200

    Functional Interface Example

--- src/main/java/com/java8/refresher/JavaConcepts/App.java
+++ src/main/java/com/java8/refresher/JavaConcepts/App.java
@@ -1,5 +1,12 @@
 package com.java8.refresher.JavaConcepts;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
 /**
  * Hello world!
  *
@@ -8,6 +15,74 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println("Test Functional Interfaces like Predicate, Cosumer,Function,Supplier");
+        
+        List<Student> listStudents = populateStudentData();
+        
+        //Predicate test : list students with mathematics as subject
+        
+        Predicate<Student> mathematicsPredicate = (Student s) -> s.getSpecialization().equalsIgnoreCase("Mathematics");
+        
+        for(Student s : listStudents) {
+        	if(mathematicsPredicate.test(s)) {
+        		System.out.println(s);
+        	}
+        }
+        
+        //Consumer interface accept(T t) method
+        
+        Consumer<Student> studentConsumer = (Student s) -> System.out.println(s.getName() + s.getPercentage());
+        		
+        for(Student s : listStudents) {
+        	studentConsumer.accept(s);
+        }
+        
+        //Function interface
+        
+        Function<Student,String> studentFunction = (Student s ) -> s.getName();
+        
+        for(Student s : listStudents) {
+        	System.out.println(studentFunction.apply(s));
+        }
+        
+       //Supplier functional interface
+        
+        Supplier<Student> studentSupplier = () -> new Student(111111, "Mahesh", 94.6, "History");
+        
+        listStudents.add(studentSupplier.get());
+        
+        System.out.println(listStudents);
+        
     }
+    
+    public static List<Student> populateStudentData(){
+    	
+        List<Student> listOfStudents = new ArrayList<Student>();
+        
+        listOfStudents.add(new Student(111, "John", 81.0, "Mathematics"));
+                 
+        listOfStudents.add(new Student(222, "Harsha", 79.5, "History"));
+                 
+        listOfStudents.add(new Student(333, "Ruth", 87.2, "Computers"));
+                 
+        listOfStudents.add(new Student(444, "Aroma", 63.2, "Mathematics"));
+                 
+        listOfStudents.add(new Student(555, "Zade", 83.5, "Computers"));
+                 
+        listOfStudents.add(new Student(666, "Xing", 58.5, "Geography"));
+                 
+        listOfStudents.add(new Student(777, "Richards", 72.6, "Banking"));
+                 
+        listOfStudents.add(new Student(888, "Sunil", 86.7, "History"));
+                 
+        listOfStudents.add(new Student(999, "Jordan", 58.6, "Finance"));
+                 
+        listOfStudents.add(new Student(101010, "Chris", 89.8, "Computers"));
+        
+        return listOfStudents;
+    	
+    }
+    
+    
+    
 }
